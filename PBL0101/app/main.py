17eb64a9b3728495716c5c405354b645e1f55f04
sobahus <sobahusn27@gmail.com>
2026-09-22 23:46:56 +0700

    fix: convert submodules into regular tracked folders

---
+++ PBL0101/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title="Simple Project Hello World using FastAPI")
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "title": "Tugas PBL 0101 - 054",
+            "message": "Hello World",
+            "description": "Simple Project Hello World using FastAPI",
+        },
+    )
